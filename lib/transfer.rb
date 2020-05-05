a65975c57cfbf0e2ba9d4e2e471f95dd180e4411
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-05 17:15:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,14 +23,7 @@ class Transfer
   end
   
   def execute_transaction
-    if self.valid?
-      @sender.withdrawl(amount)
-      @receiver.deposit(amount)
-      @status = 'complete'
-    else
-      @status = 'rejected'
-      "Transaction rejected. Please check your account balance."
-    end
+    
   end
   
 end
