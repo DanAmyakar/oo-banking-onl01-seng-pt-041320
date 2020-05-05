ee83d33c0c650f7d127f0ece5e24a68541bcb21f
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-05 15:59:12 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -22,7 +22,7 @@ class BankAccount
   end
   
   def valid?
-    !(self.balance > 0 && self.status = 'open') ? true : false
+    (self.status = 'open' && self.balance > 0) ? true : false
   end
   
   def close_account
