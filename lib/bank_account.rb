f58b3be679c3fb529857382086f5efbad8962b7a
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-05 15:40:23 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,5 +9,16 @@ class BankAccount
     @balance = 1000
     @status = 'open'
   end
+  
+  def deposit(qnty_to_deposite)
+    self.balance += qnty_to_deposite
+    self.balance
+  end
+  
+  def display_balance
+    self.balance
+  end
+  
+  
 
 end
