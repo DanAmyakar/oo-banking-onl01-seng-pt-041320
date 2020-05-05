13954c8a1763cbd58adc2cdd0a370ab706769231
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-05 17:58:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,13 +19,26 @@ class Transfer
   end
   
   def valid?
-    (sender.valid? && receiver.valid?) ? true : false
+    valid_flag = false
+    if (sender.valid? == true && receiver.valid? == true)
+      valid_flag = true if sender.balance > amount
+    else
+      valid_flag
+    end
   end
   
   def execute_transaction
     # ensures each xfer can only happen once
     # must valid? both accounts
-    
+    # upon a succesful xfer change status to complete
+    if @@all.none?{|xfer| xfer == self}
+      if self.valid? == true
+        @sender.withdrawl -= @amount
+        @receiver.deposite += @amount
+        @status = 'complete'
+      else
+        @status = 'rejected'
+        ''
   end
   
 end
